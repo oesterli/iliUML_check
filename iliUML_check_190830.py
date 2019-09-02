ae8a25af026aded53b10d105f8b9be75d81fb8ea
oesterli <34167552+oesterli@users.noreply.github.com>
2019-09-02 11:26:45 +0200

    Update iliUML_check_190830.py
    Ergänzungen an der Ausgabe in logFile

--- iliUML_check_190830.py
+++ iliUML_check_190830.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
-#import re
+
 
 # Open files
-outFile = open("data/Asset_CHBase_mod.uml", "w")
+outFile = open("data/seismic/DM_2D-Seismic_190702_mod.uml", "w")
+log = open("data/seismic/log.tsv", "w")
 
 # Define Variables
 line_A = 'emptyLine_A'
 line_A = 'emptyLine_B'
 i = 1
+count = 0
 regex = '<Entry TID='
+log.write('LineNum' + '\t' + 'ActionType' + '\t' + 'lineString' + '\t' + 'CurrentCount' + '\n')
 
-with open('data/Asset_CHBase_orig.uml') as inFile:
+with open('data/seismic/DM_2D-Seismic_190702_orig.uml') as inFile:
     for line in inFile:
+        # If the current line starts with the string defined in "regex" do the following
         if line.startswith(regex):
             firstLine = line
             secondLine = next(inFile)
 
+            # If the consecutive line starts with the string defined in "regex" do the following
             if secondLine.startswith(regex):
                 ######
                 # TESTING: Write each line to outFile
@@ -29,6 +34,11 @@ with open('data/Asset_CHBase_orig.uml') as inFile:
                 # PRODUCTIVE: Write second line to outFile
                 outFile.write(secondLine)
 
+                # Write to log-file
+                count += 1
+                log.write(str(i) + '\t' + 'First line deleted.' + '\t' + firstLine + '\t' + str(count) + '\n')
+                i += 2
+
             else:
                 ######
                 # TESTING: Write each line to outFile
@@ -41,7 +51,12 @@ with open('data/Asset_CHBase_orig.uml') as inFile:
 
                 # PRODUCTIVE: Write first and second line to outFile
                 outFile.write(firstLine)
+                # count += 1
+                # log.write('First line written. Current count: ' + str(count) + '\n')
                 outFile.write(secondLine)
+                # count += 1
+                # log.write('Second line written. Current count: ' + str(count) + '\n')
+                i += 2
         else:
             ######
             # TESTING: Write each line to outFile
@@ -53,10 +68,19 @@ with open('data/Asset_CHBase_orig.uml') as inFile:
 
             # PRODUCTIVE: Write line to outFile
             outFile.write(line)
+            # count += 1
+            # log.write('Line. Current count: ' + str(count) + '\n')
+            i += 1
 
 
+
+# Write log-file
+log.write('-----------------------------------\n'
+          'Total count of actions: ' + str(count) +
+          '\n-----------------------------------')
 
 
 # Close inFile and outFile
 inFile.close()
 outFile.close()
+log.close()
